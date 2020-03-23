c5611d0f22b641263f4555988bd7642e2d019c20
Ryan LaCombe <ryanalacombe@gmail.com>
2020-03-23 16:55:21 -0500

    Minor adjustments

--- raygame/Agent.h
+++ raygame/Agent.h
@@ -1,5 +1,12 @@
 #pragma once
 class Agent
 {
+	Agent() {}
+	virtual ~Agent() {}
+
+	//Update the agent and its behaviors
+	virtual void update(float deltaTime);
+
+	//Add a behavior to the agent
 };
 
--- raygame/KeyboardBehavior.cpp
+++ raygame/KeyboardBehavior.cpp
@@ -4,19 +4,19 @@ Vector2 KeyboardBehavior::update(Agent* agent, float deltaTime)
 {
 	Vector2 force = { 0, 0 };
 
-	if (IsKeyDown(KEY_UP))
+	if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W))
 	{
 		force.y = -500.0f;
 	}
-	if (IsKeyDown(KEY_DOWN))
+	if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S))
 	{
 		force.y = 500.0f;
 	}
-	if (IsKeyDown(KEY_LEFT))
+	if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
 	{
 		force.x = -500.0f;
 	}
-	if (IsKeyDown(KEY_RIGHT))
+	if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
 	{
 		force.x = 500.0f;
 	}
